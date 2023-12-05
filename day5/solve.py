62dc50e3d211830cede91b74dea231e29b515c29
Christoph Meißner <github@christophmeissner.de>
2023-12-05 17:36:36 +0100

    Day 5
    add day 5

---
+++ day5/solve.py
@@ -0,0 +1,123 @@
+from utils import batched, offset_range, read_lines
+
+
+def make_mapping(dest, source, length):
+    def mapping(value):
+        # print(dest, source, length)
+        if source <= value < source + length:
+            return value - (source - dest)
+
+    mapping.lenght = length
+    mapping.offset = -(source - dest)
+    mapping.dest = range(dest, dest + length)
+    mapping.source = range(source, source + length)
+    mapping.source_min = source
+    mapping.source_max = source + length - 1
+    return mapping
+
+
+def part1(filename="input.txt"):
+    lines = read_lines(filename)
+    answer = 0
+    seeds = list(map(int, lines[0].split()[1:]))
+    stages = []
+    current_stage = []
+    for line in lines[2:] + [""]:
+        if line and line[0].isdigit():
+            dest, source, length = map(int, line.split())
+            current_stage.append(make_mapping(dest, source, length))
+        elif current_stage:
+            stages.append(current_stage)
+            current_stage = []
+
+    locations = []
+    for seed in seeds:
+        # print(f"seed = {seed}")
+        output = seed
+        for s_idx, stage in enumerate(stages):
+            stage_input = output
+            # print(f"seed {seed}: stage {s_idx}({stage_input})")
+            for m_idx, mapping in enumerate(stage):
+                mapped = mapping(stage_input)
+                if mapped:
+                    # print(f"seed {seed}: stage {s_idx}({stage_input}) -> {mapped}")
+                    output = mapped
+                    break
+        locations.append(output)
+        # print(f"seed = {seed} -> {output}")
+
+    answer = min(locations)
+    print(f"part1 for {filename}: {answer}")
+    return answer
+
+
+class Stage:
+    def __init__(self, name):
+        self.name = name
+        self.mappings = dict()
+
+    def add_mapping(self, dest, source, length):
+        r = range(source, source + length)
+        self.mappings[r] = -(source - dest)
+
+    def remap_ranges(self, seed_ranges):
+        seed_ranges = seed_ranges.copy()
+        output = dict()
+        while len(seed_ranges):
+            i_r, i_o = seed_ranges.popitem()
+            for m_r, m_o in self.mappings.items():
+                mapped_range = unmapped_range = None
+                if i_r.start in m_r and i_r[-1] in m_r:
+                    mapped_range = range(i_r.start, i_r.stop)
+                elif i_r.start in m_r:
+                    mapped_range = range(i_r.start, m_r.stop)
+                    unmapped_range = range(m_r.stop, i_r.stop)
+                elif i_r[-1] in m_r:
+                    mapped_range = range(m_r.start, i_r.stop)
+                    unmapped_range = range(i_r.start, m_r.start)
+                if mapped_range:
+                    output[offset_range(mapped_range, m_o)] = i_o + m_o
+                    if unmapped_range:
+                        seed_ranges[unmapped_range] = i_o
+                    break
+            else:
+                output[i_r] = i_o
+        return output
+
+
+def part2(filename="input.txt"):
+    lines = read_lines(filename)
+
+    seed_ranges = dict(
+        (range(start_seed, start_seed + length), 0)
+        for start_seed, length in batched(map(int, lines[0].split()[1:]), 2)
+    )
+
+    stages = []
+    current_stage = None
+    for line in lines[2:] + [""]:
+        if line:
+            if line[0].isdigit():
+                dest, source, length = map(int, line.split())
+                current_stage.add_mapping(dest, source, length)
+            else:
+                current_stage = Stage(name=line.split()[0])
+        else:
+            stages.append(current_stage)
+            current_stage = None
+
+    for stage in stages:
+        seed_ranges = stage.remap_ranges(seed_ranges)
+
+    result = min(map(lambda i: i[0].start, seed_ranges.items()))
+    print(f"part2 for {filename}: {result}")
+    return result
+
+
+if __name__ == "__main__":
+    assert part1("example1.txt") == 35
+    assert part1() == 535088217
+
+    assert part2("example2.txt") == 35
+    assert part2("example1.txt") == 46
+    assert part2() == 51399228
--- utils.py
+++ utils.py
@@ -1,3 +1,41 @@
+import itertools
+from time import perf_counter
+
+
 def read_lines(filename):
     with open(filename) as inputfile:
-        return list(l.strip() for l in inputfile)
+        return list(line.strip() for line in inputfile)
+
+
+def batched(iterable, n):
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError("n must be at least one")
+    it = iter(iterable)
+    while batch := tuple(itertools.islice(it, n)):
+        yield batch
+
+
+def offset_range(range_in, offset):
+    return range(range_in.start + offset, range_in.stop + offset)
+
+
+class perf_timer:
+    def __init__(self, name=None, verbose=False):
+        self.name = name
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = perf_counter()
+        self.end = None
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end = perf_counter()
+        if self.verbose:
+            print(f"Time: {self.elapsed:.10f} seconds")
+
+    @property
+    def elapsed(self):
+        p = perf_counter()
+        return (self.end or p) - self.start
