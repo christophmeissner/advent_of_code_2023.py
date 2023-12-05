13d85f64bc9c9e27b1b457c3b9ac4bce32210b4e
Christoph Meißner <github@christophmeissner.de>
2023-12-05 13:46:57 +0100

    Day 2, part 2

--- day2/day2.py
+++ day2/day2.py
@@ -20,5 +20,23 @@ def part1():
         return total
 
 
+def part2():
+    with open("input.txt") as inputfile:
+        total = 0
+        for line in (l.strip() for l in inputfile):
+            game, draws = line.split(": ")
+            draws = draws.split("; ")
+            gamenum = int(game.split(" ")[-1])
+            max_colors = dict(red=0, green=0, blue=0)
+            for drawing in draws:
+                for count, color in (d.split(" ") for d in drawing.split(", ")):
+                    max_colors[color] = max(max_colors[color], int(count))
+            power = max_colors["red"] * max_colors["green"] * max_colors["blue"]
+            print(f"{gamenum} | {draws} | {max_colors} | {power}")
+            total += power
+        return total
+
+
 if __name__ == "__main__":
     print(f"part 1: {part1()}")
+    print(f"part 2: {part2()}")
